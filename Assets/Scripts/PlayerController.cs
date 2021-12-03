92cf43dc68b66af2d92a11f0ffbc3f4ed96dacc8
Victor Kalnik <v.kalnik@mail.ru>
2021-12-03 18:19:58 +0300

    Moving the Camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    
+    private Rigidbody _rb;
+    private float _movementX;
+    private float _movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        _movementX = movementVector.x;
+        _movementY= movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        
+        _rb.AddForce(movement * speed);
+    }
+
+}
